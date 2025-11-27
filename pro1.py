9a7c11d70052827cdc6921ed03ade6e8bb52650b
amruta13gadekar <ammugadekar8@gmail.com>
2025-11-27 14:12:59 +0530

    Add average calculation and grading system
    The script calculates the average of five numbers and assigns a grade based on the average. It accepts command line arguments for the numbers or uses default values if none are provided.

---
+++ pro1.py
@@ -0,0 +1,32 @@
+import sys
+if len(sys.argv) ==6:
+  script_name=sys.argv[0]
+  n1=sys.argv[1]
+  n1=sys.argv[2]
+  n1=sys.argv[3]
+  n1=sys.argv[4]
+  n1=sys.argv[5]
+else:
+  n1=80
+  n2=20
+  n3=50
+  n4=40
+  n5=90
+  print("No input given-using default values:")
+
+average = (n1+n2+n3+n4+n5)/5
+
+if average >= 90:
+    grade = "A"
+elif average >= 75:
+    grade = "B"
+elif average >= 60:
+    grade = "C"
+elif average >= 40:
+    grade = "D"
+else:
+    grade = "Fail"
+
+print("Average Marks:", average)
+print("Grade:", grade)
+print("Script Name", script_name)
